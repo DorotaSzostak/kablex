0db5b0b5f10d08f42e4063cdfec47cdccb0e7ebc
dorotaszostak <dorota.szostak@gmail.com>
2017-09-29 17:44:57 +0200

    updating project

---
+++ karuzela.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.container__slider').slick({
+      autoplay:true,
+      arrows: false,
+      dots: true,
+      appendDots: $(".slick__dots")
+  });
+});
+
+
+	
